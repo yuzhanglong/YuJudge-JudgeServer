e5ddb17f3ce7eaa7f39b7457f45a959843a59c67
yuzhanglong <yuzl1123@163.com>
2020-07-17 12:05:28 +0800

    项目初始化

---
+++ src/main/java/com/yzl/judgeServer/YuJudgeJudgeServerApplication.java
@@ -0,0 +1,16 @@
+package com.yzl.judgeServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yuzhanglong
+ * @description YuJudge的服务端项目
+ * @version develop
+ */
+@SpringBootApplication
+public class YuJudgeJudgeServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YuJudgeJudgeServerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/yzl/judgeServer/YuJudgeJudgeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzl.judgeServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuJudgeJudgeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
